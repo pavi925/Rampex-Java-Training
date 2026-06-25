5ddaaf41ac8cea418e0bce453c7f47f5d325a90e
Pavithra <pavithraarumugam925@gmail.com>
2026-06-25 15:21:37 +0530

    this program contains the schedule of a week using loop

---
+++ src/ClassWork/Day_6/Schedule.java
@@ -0,0 +1,13 @@
+package ClassWork.Day_6;
+
+public class Schedule {
+    public static void main(String[]args){
+        for(int week =1;week<=4;week++){
+            for (int day=1;day<=7;day++){
+                System.out.println("week" + week + "day"+ day);
+                System.out.println("come to college"+"study and go home");
+
+            }
+        }
+    }
+}
