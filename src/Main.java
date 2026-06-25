6e4646792dfb1c52478195c3e84d16295b751fda
Pavithra <pavithraarumugam925@gmail.com>
2026-06-25 10:01:52 +0530

    Project created

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int a = 6;
+        int b = 7;
+        int result = a+b;
+        System.out.println("ADD: "+ result);
+    }
+}
