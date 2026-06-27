4c78b01593bc68918c04b8f44d519024b8a24ebe
Pavithra <pavithraarumugam925@gmail.com>
2026-06-27 12:14:53 +0530

    sum of arrays

--- src/Day_7/ClassWork/SumArray.java
+++ src/Day_7/ClassWork/SumArray.java
@@ -3,8 +3,6 @@ package Day_7.ClassWork;
 import java.util.Scanner;
 public class SumArray {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
         int sum = 0;
         for(int i = 0;i<5;i++){
             sum += i;
