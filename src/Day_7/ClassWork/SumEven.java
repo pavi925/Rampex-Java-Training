7353d86fb17675bcc23f9682c2b88f838a73c11c
Pavithra <pavithraarumugam925@gmail.com>
2026-06-27 15:20:14 +0530

    Program to sum even numbers

---
+++ src/Day_7/ClassWork/SumEven.java
@@ -0,0 +1,10 @@
+package Day_7.ClassWork;
+public class SumEven {
+    public static void main(String[] args){
+        int sum = 0;
+        for(int i = 0;i<5;i++){
+            sum += i;
+        }
+        System.out.println("Sum:" + sum);
+    }
+}
