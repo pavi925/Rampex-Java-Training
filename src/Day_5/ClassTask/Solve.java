b2393da46178e41113a8c1dd4aaf8a58ef6273a6
Pavithra <pavithraarumugam925@gmail.com>
2026-06-28 21:37:29 +0530

    Program to print consecutive numbers using loop

---
+++ src/Day_5/ClassTask/Solve.java
@@ -0,0 +1,9 @@
+package Day_5.ClassTask;
+
+public class Solve{
+    public static void main(String[] args){
+        for(int i = 0; i < 7; i++) {
+            System.out.println( i);
+        }
+    }
+}
