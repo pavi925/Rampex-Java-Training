b6cacd339b6b1cab9dc88008486cce152b5eead9
Pavithra <pavithraarumugam925@gmail.com>
2026-06-28 21:21:17 +0530

    Program to add by using loop expressions

---
+++ src/Day_4/HomeTask/Add.java
@@ -0,0 +1,9 @@
+package Day_4.HomeTask;
+
+public class Add{
+    public static void main(String[] args){
+        for(int i = 1; i <= 5; i++) {
+            System.out.print(i++ + " ");
+        }
+    }
+}
