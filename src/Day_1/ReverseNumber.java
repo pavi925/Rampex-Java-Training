2d8003a5dd3e71c7a4b00b44b11bebbaefd7d5c5
Pavithra <pavithraarumugam925@gmail.com>
2026-06-28 20:54:07 +0530

    Program to reverse a number

---
+++ src/Day_1/ReverseNumber.java
@@ -0,0 +1,16 @@
+package Day_1;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 123;
+        int reversed = 0;
+
+        int  ones = num % 10;
+        int tens = (num / 10) % 10;
+        int hundreds = num / 100;
+        reversed = ones * 100 + tens * 10 + hundreds;
+
+        System.out.println("Original number: " + num);
+        System.out.println("Reversed number: " + reversed);
+    }
+}
