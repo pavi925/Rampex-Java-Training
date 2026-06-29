1e14fed5d74a057a5065ac68b97e63b849ce86e3
Pavithra <pavithraarumugam925@gmail.com>
2026-06-29 07:17:09 +0530

    Program to add the digits

---
+++ src/Day_7/HomeWork/SumOfDigits.java
@@ -0,0 +1,16 @@
+package Day_7.HomeWork;
+
+public class SumOfDigits {
+    public static void main(String[] args){
+        int num =543;
+        int sum=0;
+        while(num>0){
+            int digit=num%10;
+            sum+=digit;
+            num/=10;
+
+
+        }
+        System.out.print(sum);
+    }
+}
