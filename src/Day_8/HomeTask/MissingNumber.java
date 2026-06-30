ed47852913860b45ab210fbda51e1a947dfdbe0c
Pavithra <pavithraarumugam925@gmail.com>
2026-06-30 11:38:16 +0530

    program to find the missing number

--- src/Day_8/HomeTask/MissingNumber.java
+++ src/Day_8/HomeTask/MissingNumber.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class MissingNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number:");
+        System.out.print("Enter the number: ");
         int n = sc.nextInt();
         int[] arr = new int[n - 1];
         for (int i = 0; i < n - 1; i++) {
