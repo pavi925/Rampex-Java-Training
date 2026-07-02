dc232ce27aa19f5cb0a349e625ab9805a518ff8c
Pavithra <pavithraarumugam925@gmail.com>
2026-07-02 07:29:43 +0530

    printing the duplicate strings

--- src/Day_10/ClassTask/PrintingDuplicates.java
+++ src/Day_10/ClassTask/PrintingDuplicates.java
@@ -5,7 +5,7 @@ public class PrintingDuplicates {
         String [] arr = {"sam","Jane","sam","tom","sam"};
         for(int i = 0;i<arr.length;i++){
             for(int j = 0;j<arr.length;j++){
-                if(arr[i].equals(arr[j]) && i!=j && i<j){
+                if(arr[i].equals(arr[j])  && i<j && i!=j){
                     System.out.println(arr[j]);
                     break;
                 }
