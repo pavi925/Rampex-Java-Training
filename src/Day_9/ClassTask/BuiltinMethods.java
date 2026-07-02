c26f732002e3728558052744f7248763a360cf5b
Pavithra <pavithraarumugam925@gmail.com>
2026-07-02 07:23:41 +0530

    program for built in methods

---
+++ src/Day_9/ClassTask/BuiltinMethods.java
@@ -0,0 +1,14 @@
+package Day_9.ClassTask;
+
+public class BuiltinMethods {
+
+    public static void main(String [] args){
+        String  name ="Mazza";
+
+//       System.out.println(name.toUpperCase());
+//       System.out.println(name);
+//       System.out.println(name.replaceAll("M","L"));
+        System.out.println(name.equalsIgnoreCase("mazza"));
+
+    }
+}
