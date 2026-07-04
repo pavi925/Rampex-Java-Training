a9c14aa589838e8644330e4595b158bd5385f53f
Pavithra <pavithraarumugam925@gmail.com>
2026-07-04 11:40:36 +0530

    assignment expression

---
+++ src/Day_11/HomeTask/Assignment.java
@@ -0,0 +1,11 @@
+package Day_11.HomeTask;
+
+public class Assignment {
+    public static void main(String[] args){
+        int a = 10;
+        int b = (a = 20) + a;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
---
+++ src/Day_11/HomeTask/AssignmentExpression.java
@@ -0,0 +1,11 @@
+package Day_11.HomeTask;
+
+public class AssignmentExpression {
+    public static void main(String[] args){
+        int x = 5;
+        if (x > 10 && ++x > 5)
+        {
+        }
+        System.out.println(x);
+    }
+}
