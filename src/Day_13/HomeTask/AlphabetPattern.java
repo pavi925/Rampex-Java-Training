0e7a49edca0738d36d58cb32d8527cce8c945171
Pavithra <pavithraarumugam925@gmail.com>
2026-07-05 20:48:06 +0530

    To print Alphabet pattern

--- src/Day_13/HomeTask/AlphabetPattern.java
+++ src/Day_13/HomeTask/AlphabetPattern.java
@@ -5,8 +5,7 @@ public class AlphabetPattern {
         int rows = 6;
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= rows - i; j++) {
-                System.out.print(" ");
-            }
+                System.out.print(" ");}
             for (int j = 0; j < i; j++) {
                 System.out.print((char)('A' + j) + " ");
             }
