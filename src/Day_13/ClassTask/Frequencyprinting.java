dab8738bebd82e54d9f3896ed0b2a83d87910291
Pavithra <pavithraarumugam925@gmail.com>
2026-07-05 20:47:26 +0530

    To check the pangram

---
+++ src/Day_13/ClassTask/Frequencyprinting.java
@@ -0,0 +1,22 @@
+package Day_13.ClassTask;
+
+
+public class Frequencyprinting {
+    public static void main(String[] args) {
+        String name = "Good";
+
+        int count = 0;
+        for (int i = 0; i < name.length(); i++) {
+            for (int j = 0; j < name.length(); j++) {
+                if (name.charAt(i) == name.charAt(j)) {
+                    count++;
+                }
+            }
+            System.out.println(name.charAt(i) +": "+ count);
+            count =0;
+
+
+
+        }
+    }
+}
