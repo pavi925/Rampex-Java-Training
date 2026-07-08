6646217d2458c9e7dce0e908948ffd1b3abd850e
Pavithra <pavithraarumugam925@gmail.com>
2026-07-08 12:13:01 +0530

    Project created

--- src/Day_1/ReverseNumber.java
+++ src/Day_1/ReverseNumber.java
@@ -3,7 +3,6 @@ package Day_1;
 public class ReverseNumber {
     public static void main(String[] args) {
         int num = 123;
-
         int  ones = num % 10;
         int tens = (num / 10) % 10;
         int hundreds = num / 100;
