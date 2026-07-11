91e0eaae9893fac1cfb10c0d7827d9f4db4054c7
Pavithra <pavithraarumugam925@gmail.com>
2026-07-11 09:42:35 +0530

    Removing duplicates

---
+++ src/Day_17/ClassTask/RemovingDuplicatesUsingSet.java
@@ -0,0 +1,19 @@
+package Day_17.ClassTask;
+
+import java.util.Arrays;
+import java.util.HashSet;
+public class RemovingDuplicatesUsingSet {
+    public static void main(String[] args) {
+        int [] arr ={1,2,3,2,4,3,1};
+        HashSet<Integer> hs =new HashSet<>();
+        for(Integer i : arr){
+            hs.add(i);
+        }
+        int [] nums = new int[hs.size()];
+        int index =0;
+        for (Integer i : hs){
+            nums[index++]=i;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
