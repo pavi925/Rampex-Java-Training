a22224eea0a97192330b284ac85dad22e7ea65ae
Pavithra <pavithraarumugam925@gmail.com>
2026-07-15 07:35:57 +0530

    Sum of even numbers using recursion

---
+++ src/Day_21/HomeTask/SreamAPI/SumOfEven.java
@@ -0,0 +1,15 @@
+package Day_21.HomeTask.SreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SumOfEven{
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        int sum = list.stream()
+                .filter(num -> num % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println(sum);
+    }
+}
