3c72afb6c016fc775c42da6b5572bd8f1f040f44
Pavithra <pavithraarumugam925@gmail.com>
2026-07-15 07:36:16 +0530

    Sum using recursion

---
+++ src/Day_21/ClassTask/SumUsingRecursion.java
@@ -0,0 +1,19 @@
+package Day_21.ClassTask;
+
+public class SumUsingRecursion {
+    public static void main(String [] args){
+        int sum = 0;
+        adder(1,sum);
+
+    }
+    public static void adder(int num,int sum){
+
+       if(num == 5){
+           return ;
+       }
+       sum = sum +num;
+       num++;
+       System.out.println(sum);
+        adder(num,sum);
+    }
+}
