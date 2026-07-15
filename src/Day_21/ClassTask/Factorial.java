51cf9c3d101de84ae9f7ef9173995e12ad415b7c
Pavithra <pavithraarumugam925@gmail.com>
2026-07-15 07:33:52 +0530

    To find factorial using recursion

---
+++ src/Day_21/ClassTask/Factorial.java
@@ -0,0 +1,17 @@
+package Day_21.ClassTask;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fact(n));
+
+
+    }
+
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+}
