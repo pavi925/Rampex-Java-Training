76cb0dd0f7141266b4e9850955a1a42b1f388947
Pavithra <pavithraarumugam925@gmail.com>
2026-07-15 07:36:52 +0530

    Recursion

---
+++ src/Day_21/ClassTask/RecursionDemo.java
@@ -0,0 +1,17 @@
+package Day_21.ClassTask;
+
+public class RecursionDemo {
+    public static void main(String [] args){
+        int a = 5;
+        fun(a);
+
+    }
+    public static void fun(int a){
+       if(a == 0){
+           return;
+        }
+       System.out.println(a);
+        a--;
+       fun(a);
+    }
+}
